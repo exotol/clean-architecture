d976477207fb1c51bfefcd3f2cebc73ebbceb0a1
artem.bardakov <artem.bardakov@gmail.com>
2025-12-25 13:07:24 +0300

    [UPD] убрал использование внутренних конфигов прометеуса

--- src/app/presentation/api/common/endpoints/metrics.py
+++ src/app/presentation/api/common/endpoints/metrics.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
 from fastapi import Response
-from opentelemetry import metrics
-from opentelemetry.exporter.prometheus import PrometheusMetricReader
-from opentelemetry.sdk.metrics import MeterProvider
 from prometheus_client import generate_latest
+
 
 router = APIRouter()
 
@@ -13,13 +11,6 @@ def get_metrics() -> Response:
     """
     Expose Prometheus metrics.
     """
-    # Force collection of metrics
-    provider = metrics.get_meter_provider()
-    if isinstance(provider, MeterProvider):
-        for reader in provider._sdk_config.metric_readers:
-            if isinstance(reader, PrometheusMetricReader):
-                reader.collect()
-
     return Response(
         content=generate_latest(),
         media_type="text/plain",
--- tests/e2e/api/test_metrics_endpoint.py
+++ tests/e2e/api/test_metrics_endpoint.py
@@ -17,7 +17,7 @@ async def test_metrics_endpoint(client: AsyncClient) -> None:
     response = await client.get("/common/metrics")
     assert response.status_code == 200
     assert response.headers["content-type"] == "text/plain; charset=utf-8"
-    
+
     # 3. Verify content
     content = response.text
     assert METRICS_REQUESTS_TOTAL_NAME in content
