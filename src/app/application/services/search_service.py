e335402a6e792714dafd9ebaa94e092673bcd9a3
artem.bardakov <artem.bardakov@gmail.com>
2025-12-27 00:10:25 +0300

    [UPD] добавил явную реализацию интерфейса репозитория (не смотря на duck typing)

--- src/app/application/services/search_service.py
+++ src/app/application/services/search_service.py
@@ -1,5 +1,3 @@
-from loguru import logger
-
 from app.domain.entities.document import Document
 from app.domain.interfaces.search_repository import ISearchRepository
 from app.infrastructure.observability.events import Events
@@ -16,5 +14,4 @@ class SearchService:
         use_log_result=True,
     )
     async def search(self, query: str) -> list[Document]:
-        logger.info(query)
         return await self._repository.search(query=query)
--- src/app/infrastructure/persistence/repositories/search_repository.py
+++ src/app/infrastructure/persistence/repositories/search_repository.py
@@ -1,7 +1,10 @@
 from app.domain.entities.document import Document
 
 
-class SearchRepository:
+from app.domain.interfaces.search_repository import ISearchRepository
+
+
+class SearchRepository(ISearchRepository):
     async def search(self, query: str) -> list[Document]:  # noqa: PLR6301
         # Mock implementation
         # In a real scenario, this would call OpenSearch/Elasticsearch
